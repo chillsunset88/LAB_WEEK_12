e4edcb7f56f447de735270c5d38cc47ad6dca7e1
Geoffrey <geoffreyraditya12@gmail.com>
2025-12-12 21:21:05 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -3,11 +3,14 @@ package com.example.test_lab_week_12
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.example.test_lab_week_12.MovieRepository
 import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+
+
 
 
 class MovieViewModel(private val movieRepository: MovieRepository)
@@ -16,19 +19,30 @@ class MovieViewModel(private val movieRepository: MovieRepository)
         fetchPopularMovies()
     }
     // define the LiveData
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
-    val error: LiveData<String>
-        get() = movieRepository.error
+    private val _popularMovies = MutableStateFlow(
+        emptyList<Movie>()
+    )
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
     // fetch movies from the API
     private fun fetchPopularMovies() {
 // launch a coroutine in viewModelScope
-// Dispatchers.IO means that this coroutine will run on a shared       pool of threads
+// Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies()
+            movieRepository.fetchMovies().catch {
+// catch is a terminal operator that catches exceptions from the Flow
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect {
+// collect is a terminal operator that collects the values from the Flow
+// the results are emitted to the StateFlow
+                _popularMovies.value = it
+            }
         }
     }
 }
+
+
 
 class MovieViewModelFactory(private val repository: MovieRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
